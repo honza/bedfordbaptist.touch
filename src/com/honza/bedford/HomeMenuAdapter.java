b7cbe4b4af80d249432cbe6df5689b2160462992
Honza Pokorny <me@honza.ca>
2011-02-05 21:34:47 -0400

    Remove unused import

--- src/com/honza/bedford/HomeMenuAdapter.java
+++ src/com/honza/bedford/HomeMenuAdapter.java
@@ -8,7 +8,6 @@ import android.widget.BaseAdapter;
 import android.widget.GridView;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
-import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 public class HomeMenuAdapter extends BaseAdapter {
