11a9ec05fa7528d521c7659c02ff8ee6391c04e8
Honza Pokorny <me@honza.ca>
2011-02-06 16:11:02 -0400

    Add simple NewsItem fetching from www.

---
+++ src/com/honza/bedford/Downloader.java
@@ -0,0 +1,59 @@
+package com.honza.bedford;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.JSONValue;
+
+import android.util.Log;
+
+public class Downloader {
+	
+	private final String newsUrl = "http://www.bedfordbaptist.ca/wp-content/plugins/android/news.php";
+	private String res = "";
+	
+	public List<NewsItem> getNews(){
+		String json = this.getJSON(this.newsUrl);
+		Object obj = JSONValue.parse(json);
+		JSONArray array = (JSONArray) obj;
+		int size = array.size();
+		List<NewsItem> list = new ArrayList<NewsItem>();
+		
+		for (int i=0; i < size; i++){
+			JSONObject x = (JSONObject) array.get(i);
+			String author = x.get("post_author").toString();
+			String title = x.get("post_title").toString();
+			String post_date = x.get("post_date").toString();
+			NewsItem item = new NewsItem(author, title, post_date);
+			list.add(item);
+		}
+		
+		return list;
+	}
+	
+	private String getJSON(String url){
+		String line = "";
+		try {
+			URL u = new URL(url);
+			URLConnection conn = u.openConnection();
+			conn.setDoInput(true);
+			InputStream in = conn.getInputStream();
+			BufferedReader input = new BufferedReader(new InputStreamReader(in));
+			while ((line = input.readLine()) != null){
+				res = res + line;
+			}
+			
+		} catch (Exception e) {
+			Log.v("Bedford", e.toString());
+		}
+		return res;
+	}
+
+}
--- src/com/honza/bedford/NewsActivity.java
+++ src/com/honza/bedford/NewsActivity.java
@@ -1,25 +1,48 @@
 package com.honza.bedford;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import android.app.ListActivity;
+import android.app.ProgressDialog;
+import android.os.AsyncTask;
 import android.os.Bundle;
 
 public class NewsActivity extends ListActivity {
 	
-	private List<NewsItem> items;
-	private NewsListAdapter adapter;
+	private static NewsListAdapter adapter;
 	
 	public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.news);
-        items = new ArrayList<NewsItem>();
-        NewsItem it = new NewsItem("Honza", "Some cool post", "today");
-        items.add(it);
-        
-        adapter = new NewsListAdapter(this, R.layout.news_item_row, items);
-        
-        setListAdapter(this.adapter);
+        setContentView(R.layout.white_screen);
+        new DownloadNewsTask().execute();
+	}
+	
+	public void updateList(){
+		setListAdapter(adapter);
+	}
+	
+	private class DownloadNewsTask extends AsyncTask<Void, Void, List<NewsItem>>{
+		
+		private final ProgressDialog dialog = new ProgressDialog(NewsActivity.this);
+
+		protected void onPreExecute(){
+			this.dialog.setMessage("Loading...");
+			this.dialog.show();
+		}
+		
+		protected void onPostExecute(List<NewsItem> items){
+			if (this.dialog.isShowing()) {
+				this.dialog.dismiss();
+			}
+			NewsActivity.adapter = new NewsListAdapter(NewsActivity.this, R.layout.news_item_row, items);
+			NewsActivity.this.updateList();
+		}
+
+		@Override
+		protected List<NewsItem> doInBackground(Void... params) {
+			Downloader d = new Downloader();
+			return d.getNews();
+		}
+		
 	}
 }
